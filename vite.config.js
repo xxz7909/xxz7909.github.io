596ae71d62fd50feb07a3221919d327a86aaace7
xxz7909 <2990698929@qq.com>
2026-01-18 10:28:13 +0800

    deploy

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './' // Optimization for relative paths on GitHub Pages
-})
